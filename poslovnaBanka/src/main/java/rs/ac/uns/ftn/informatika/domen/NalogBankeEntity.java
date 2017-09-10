8f4bb6c72dc98f8dbb6aa49978ff50d827a4bdd0
Luka Nikolic <nikolic.luka.ns@gmail.com>
2017-09-10 19:50:33 +0200

    Za poslovnu banku odradjene funkcionalnosti za korisnicki nalog i nalog banke

---
+++ poslovnaBanka/src/main/java/rs/ac/uns/ftn/informatika/domen/NalogBankeEntity.java
@@ -0,0 +1,69 @@
+package rs.ac.uns.ftn.informatika.domen;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import rs.ac.uns.ftn.informatika.bankarskinalog.BankarskiNalog;
+
+@Entity
+public class NalogBankeEntity {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String sifraBanke;
+	private String port;
+	private String SWIFT;
+	private String obracunskiRacun;
+	private double stanjeNaRacunu = 1000000;
+	public NalogBankeEntity() {
+	}
+	
+	public NalogBankeEntity(BankarskiNalog nalog) {
+		this.setObracunskiRacun(nalog.getRacun());
+		this.setPort(nalog.getBnkPort());
+		this.setSifraBanke(nalog.getSifraBanke());
+		this.setStanjeNaRacunu(nalog.getStanjeRacuna());
+		this.setSWIFT(nalog.getSWIFT());
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSifraBanke() {
+		return sifraBanke;
+	}
+	public void setSifraBanke(String sifraBanke) {
+		this.sifraBanke = sifraBanke;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	public String getObracunskiRacun() {
+		return obracunskiRacun;
+	}
+	public void setObracunskiRacun(String obracunskiRacun) {
+		this.obracunskiRacun = obracunskiRacun;
+	}
+	public double getStanjeNaRacunu() {
+		return stanjeNaRacunu;
+	}
+	public void setStanjeNaRacunu(double stanjeNaRacunu) {
+		this.stanjeNaRacunu = stanjeNaRacunu;
+	}
+	public String getSWIFT() {
+		return SWIFT;
+	}
+	public void setSWIFT(String sWIFT) {
+		SWIFT = sWIFT;
+	}
+	
+	
+	
+
+}
